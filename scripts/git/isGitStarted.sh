745ae9ea1aff225b3465efd5d94e702aa31479a8
EdwardOst <edward.ost@gmail.com>
2017-10-15 12:19:51 -0400

    updated

--- scripts/git/isGitStarted.sh
+++ scripts/git/isGitStarted.sh
@@ -4,17 +4,29 @@ set -e
 set -u
 
 
-declare gitHostname="${1:-localhost}"
-declare gitPort="${2:-80}"
-declare gitPath="${3:-}"
-declare searchPattern="${4:-GitLab}"
-declare userHome="${4:-/home/ec2-user}"
+declare gitUrl="${1:-http://localhost/users/sign_in}"
+declare searchPattern="${2:-GitLab}"
+declare logDir="${3:-/home/ubuntu}"
+
+[ -z "${gitUrl}" ] && echo "parameter gitUrl required: ${usage}" 1>&2 && exit 1
+
+declare usage="./isGitStarted <gitUrl> [ <searchPattern> [ <logDir> ] ]"
 
 declare sleepInterval=20
+declare logPath="${logDir}/isGitStarted.log"
+
+echo "gitUrl=${gitUrl}" 1>&2
+echo "searchPattern=${searchPattern}" 1>&2
+echo "logPath=${logPath}" 1>&2
+echo "sleepInterval=${sleepInterval}" 1>&2
 
 echo "$(date +%Y-%m-%d:%H:%M:%S) --- checking Git status..." 1>&2
-until [ "`wget -O - --timeout=5 http://${gitHostname}:${gitPort}/${gitPath} | tee -a ${userHome}/isGitStarted.log | grep '${searchPattern}'`" != "" ]; do
-    echo "$(date +%Y-%m-%d:%H:%M:%S) --- sleeping for ${sleepInterval} seconds before checking http://${gitHostname}:${gitPort}/${gitPath}" | tee -a ${userHome}/isGitStarted.log
+
+declare response
+response=$(wget -O - --timeout=5 "${gitUrl}" | grep "${searchPattern}")
+until [ -n "${response}" ]; do
+    echo "$(date +%Y-%m-%d:%H:%M:%S) --- sleeping for ${sleepInterval} seconds before checking ${gitUrl}" >> "${logPath}"
     sleep "${sleepInterval}"
+    response=$(wget -O - --timeout=5 "${gitUrl}" | grep "${searchPattern}")
 done
-echo "$(date +%Y-%m-%d:%H:%M:%S) --- Git is ready!  http://${gitHostname}:${gitPort}/${gitPath}" | tee -a ${userHome}/isGitStarted.log 1>&2
+echo "$(date +%Y-%m-%d:%H:%M:%S) --- Git is ready! ${gitUrl}" >> "${logPath}"
