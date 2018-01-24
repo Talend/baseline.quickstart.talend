b672898e68d25a1a6cc7356352ec72e17b2cf7f0
Edward Ost <eost@talend.com>
2018-01-24 14:37:58 +0000

    updated first of two commits from EdwardOst git repo

--- scripts/util/util.sh
+++ scripts/util/util.sh
@@ -7,17 +7,17 @@ set -u
 export UTIL_FLAG=1
 
 
-# read here documents into a variable
-# then use a here string to access it elsewhere
-#
-# define myvar <<EOF
-# the quick brown fox jumped over the lazy dog
-# EOF
-#
-# grep -q "txt" <<< "$myvar"
-#
+function assign() {
+    local var="${1}"
+    local value="${2}"
+    required var value
+    printf -v "${var}" '%s' "${value}"
+}
 
-define(){ IFS=$'\n' read -r -d '' "${1}" || true; }
+
+define(){
+    IFS=$'\n' read -r -d '' "${1}" || true;
+}
 
 
 function warningLog() {
@@ -25,25 +25,30 @@ function warningLog() {
     return 0
 }
 
+
 function infoLog() {
     [ -n "${INFO_LOG:-}" ] && echo "INFO: ${*} : ${FUNCNAME[*]:1}" 1>&2
     return 0
 }
 
+
 function infoVar() {
-    [ -n "${INFO_LOG:-}" ] && echo "INFO: ${FUNCNAME[*]:1} : ${1}=${!1}" 1>&2
-    return 0
+    [ -n "${INFO_LOG:-}" ] && [ -z "${!1+x}" ] && echo "INFO: ${FUNCNAME[*]:1} : undefined variable '${1}'" && return 0
+    [ -n "${INFO_LOG:-}" ] && echo "INFO: ${1}=${!1}" 1>&2 && return 0
 }
+
 
 function debugLog() {
     [ -n "${DEBUG_LOG:-}" ] && echo "DEBUG: ${FUNCNAME[*]:1} : ${*}" 1>&2
     return 0
 }
 
+
 function debugVar() {
-    [ -n "${DEBUG_LOG:-}" ] && echo "DEBUG: ${FUNCNAME[*]:1} : ${1}=${!1}" 1>&2
-    return 0
+    [ -n "${DEBUG_LOG:-}" ] && [ -z "${!1+x}" ] && echo "DEBUG: ${FUNCNAME[*]:1} : undefined variable '${1}'" && return 0
+    [ -n "${DEBUG_LOG:-}" ] && echo "DEBUG: ${FUNCNAME[*]:1} : ${1}=${!1}" 1>&2 && return 0
 }
+
 
 function debugStack() {
     if [ -n "${DEBUG_LOG:-}" ] ; then
@@ -53,9 +58,62 @@ function debugStack() {
     fi
 }
 
+
 function errorMessage() { 
     echo "ERROR: $0: ${FUNCNAME[*]:1} : ${*}" 1>&2
 }
+
+#
+# TODO: error not triggering even with return code of 1
+#
+# required works fine when invoked by itself, but when
+# it is invoked along with another test condition in the
+# parent and the validation fails in the child function
+# the child function error handler is not triggering.
+#
+# For example:
+#
+# Parent script calls a function
+#
+#     echo "## expect missing required argument bucket"
+#     create_bucket "" "us-east-1" && true
+#     echo "return ${?}"
+#
+# The parent script expects the child function to invoke
+# the required function on the args and fail.  Since the
+# parent script wants to continue processing it does an
+# and operation expecting to test the return result
+#
+# In the child function the required invocation returns 1 but for
+# some reason does not exit
+#
+#    function myfunc() {
+#        required bucket region
+#        # why is this necessary given that error handling is on?
+#        [ ! "${?}" == "0" ] && return ${?}
+#
+#
+
+function required() {
+    local arg
+    local error_message=""
+    for arg in "${@}"; do
+        if [ -z "${!arg+x}" ]; then
+            error_message="${error_message} undefined ${arg}"
+        elif [ -z "${!arg}" ]; then
+            error_message="${error_message} empty ${arg}"
+        else
+            debugLog "'${arg}' passed"
+        fi
+    done
+    [ -n "${error_message}" ] \
+        && error_message="missing required arguments:${error_message}" \
+        && echo "$0: ${FUNCNAME[*]:1}: ${error_message}" 1>&2 \
+        && debugLog "FAILED" \
+        && return 1
+    return 0
+}
+
 
 function die() {
     echo "$0: ${FUNCNAME[*]:1} : ${*}" 1>&2
@@ -69,30 +127,7 @@ function try() {
     done
     [ "${#}" -lt 1 ] && die "empty try statement"
 
-    ! "$@" && echo "$0: ${FUNCNAME[*]:1}: cannot execute: ${*}" 1>&2 && exit 111
+    ! "$@" && echo "ERROR: $0: ${FUNCNAME[*]:1}: cannot execute: ${*}" 1>&2 && exit 111
 
-    return 0
-}
-
-
-function assign() {
-    local var="${1}"
-    local value="${2}"
-    required var value
-    printf -v "${var}" '%s' "${value}"
-}
-
-
-function required() {
-
-    local arg
-    local error_message=""
-    for arg in "${@}"; do
-        [ -z "${!arg}" ] && error_message="${error_message} ${arg}"
-    done
-    [ -n "${error_message}" ] \
-        && error_message="missing required arguments:${error_message}" \
-        && echo "$0: ${FUNCNAME[*]:1}: ${error_message}" 1>&2 \
-        && exit 111
     return 0
 }
