084858145a18b2e608e34c4502dc2bde2a236d79
Edward Ost <eost@talend.com>
2018-01-31 00:22:23 +0000

    updated util.sh

--- scripts/util/util.sh
+++ scripts/util/util.sh
@@ -8,13 +8,19 @@ export UTIL_FLAG=1
 
 
 #
-# always be sure to quote both arguments when invoking this function, otherwise only the first token of value may be assigned to !_var
+# always be sure to quote both arguments when invoking this function, otherwise$
 #
 function assign() {
     local _var="${1}"
     local value="${2}"
-    required _var value
-    [ -z "${!_var+x}" ] && errorMessage "indirect reference variable '${_var}' is not defined prior to use"
+    if ! required _var value; then
+        echo "ERROR: ${BASH_SOURCE[1]##*/} ${BASH_LINENO[1]} : error in require$
+        return 1
+    fi
+    if [ -z "${!_var+x}" ]; then
+        echo "ERROR: ${BASH_SOURCE[1]##*/} ${BASH_LINENO[1]} : assign reference$
+        return 1
+    fi
     printf -v "${_var}" '%s' "${value}"
 }
 
@@ -66,9 +72,28 @@ function debugStack() {
 }
 
 
-function errorMessage() { 
-    echo "ERROR: $0: ${FUNCNAME[*]:1} : ${*}" 1>&2
+function errorMessage() {
+    echo "ERROR: ${BASH_SOURCE[-2]##*/} ${BASH_LINENO[-2]} : ${0} : ${FUNCNAME[$
 }
+
+
+function defined() {
+    local arg
+    local error_message=""
+    for arg in "${@}"; do
+        if [ -z "${!arg+x}" ]; then
+            error_message="${error_message} undefined ${arg}"
+        else
+            debugLog "'${arg}' passed"
+        fi
+    done
+    [ -n "${error_message}" ] \
+        && error_message="missing required arguments:${error_message}" \
+        && echo "$0: ${FUNCNAME[*]:1}: ${error_message}" 1>&2 \
+        && return 1
+    return 0
+}
+
 
 #
 # TODO: error not triggering even with return code of 1
@@ -120,6 +145,31 @@ function required() {
         && return 1
     return 0
 }
+
+
+
+function validate() {
+    local usage="validate <variable_ref> [ <allowed_values> ... ]"
+    if [ "${#}" == 0 ]; then
+        echo "${FUNCNAME[*]:1} : required: missing arguments: ${usage}" 1>&2
+        return 1
+    elif [ "${#}" == 1 ]; then
+        [ -z "${!1+x}" ] && errorMessage "undefined variable '${1}'" && return 1
+        [ -z "${!1}" ] && errorMessage "empty variable '${1}'" && return 1
+        debugLog "validated '${1}'"
+        return 0
+    else
+        local varname="${1}"
+        [ -z "${!varname+x}" ] && errorMessage "undefined variable '${varname}'" && return 1
+        shift 1
+        for value in "${@}"; do
+            [ "${!varname}" == "${value}" ] && debugLog "validated '${varname}'" && return 0
+        done
+        errorMessage "invalid variable '${varname}' value '${!varname}': valid values: ${*}"
+        return 1
+    fi
+}
+
 
 
 function die() {
