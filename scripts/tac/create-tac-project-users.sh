fc2f5f81821ad2e05bdd0df1d925869c137e2248
EdwardOst <edward.ost@gmail.com>
2018-02-07 20:36:05 -0500

    updated create-tac-project-users to use versionn specific defaults

--- scripts/tac/create-tac-project-users.sh
+++ scripts/tac/create-tac-project-users.sh
@@ -8,7 +8,9 @@ script_dir="${script_path%/*}"
 
 declare talend_version="${1:-}"
 declare tac_password="${2:-}"
-declare project_users_file="${3:-${script_dir}/project-users.data}"
+declare default_account_user="${3:-}"
+declare default_account_password="${4:-}"
+declare project_users_file="${5:-${script_dir}/project-users.data}"
 
 declare usage="create-tac-project-users.sh <talend_version> <tac_password> <project_users_file>"
 
@@ -24,8 +26,8 @@ chmod 750 "${metaservlet_path}"
 
 # todo: refactor to function
 
-default_account_user="security@company.com"
-default_account_password="admin"
+#default_account_user="security@company.com"
+#default_account_password="admin"
 
 # create tadmin with password from environment
 
