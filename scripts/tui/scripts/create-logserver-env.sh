9fcd19796f88f165c34049e456f6b561b056e93b
EdwardOst <edward.ost@gmail.com>
2018-02-10 11:31:40 -0500

    updated tui_config_mappings and templates

--- scripts/tui/scripts/create-logserver-env.sh
+++ scripts/tui/scripts/create-logserver-env.sh
@@ -12,5 +12,28 @@ script_dir="${script_path%/*}"
 
 [ ! -e "${script_dir}/ec2-metadata" ] && echo "ec2-metadata script must be in the same directory as create-logserver-env.sh" && exit 1
 
+function parse_metadata_result() {
+    local metadata="${1}"
+    local value="${metadata#*: }"
+    echo "${value}"
+}
+
+local_hostname=$(./ec2-metadata -h)
+local_hostname=$(parse_metadata_result "${local_hostname}")
+internal_hostname="${local_hostname}.ec2.internal"
+local_ipv4=$(./ec2-metadata -o)
+local_ipv4=$(parse_metadata_result "${local_ipv4}")
+public_hostname=$(./ec2-metadata -p)
+public_hostname=$(parse_metadata_result "${public_hostname}")
+public_ipv4=$(./ec2-metadata -v)
+public_ipv4=$(parse_metadata_result "${public_ipv4}")
+
+if [ -z "${public_ipv4}" ] || [ "${public_ipv4}" == "not available" ] || [ -z "${public_hostname}" ] || [ "${public_hostname}" == "not available" ]; then
+    echo export TALEND_LOGGING_HOST="${internal_hostname}" >> "${target_path}"
+else
+    echo export TALEND_LOGGING_HOST="${public_hostname}" >> "${target_path}"
+fi
+
+
 echo "echo server-env: logserver finished" >> "${target_path}"
 
